520d2eb78b016916f2a4c58d4ad23c98b3cd4a60
OuardaHolberton <12985@holbertonstudents.com>
2026-05-06 14:14:49 +0200

    Fix last_digit PEP8 and conditions

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
-digit = number % 10 if number >= 0 else -(abs(number)%10)
+digit = number % 10 if number >= 0 else -(abs(number) % 10)
 if digit > 5:
-    print(f"Last digit of {number} is {digit} and is greater than 5")
-elif digit < 6 and digit != 0:
-    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
+    print("Last digit of {} is {} and is greater than 5".format(number, digit))
+elif digit == 0:
+    print("Last digit of {} is {} and is 0".format(number, digit))
 else:
-    print(f"Last digit of {number} is {digit} and is 0")
+    print("Last digit of {} is {} and is less than 6 and not 0".format(
+        number, digit))
