de508d87081b72d3ff2e8b6caeb3f41f266cde79
OuardaHolberton <12985@holbertonstudents.com>
2026-05-15 11:10:39 +0200

    Fix safe_print_integer

--- python_fundamentals/exception_handling/safe_print_integer.py
+++ python_fundamentals/exception_handling/safe_print_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+"""Module for safe integer printing."""
 
 
 def safe_print_integer(value):
+    """Print an integer with {:d} format."""
     try:
+        if isinstance(value, bool):
+            return False
         print("{:d}".format(value))
         return True
     except ValueError:
